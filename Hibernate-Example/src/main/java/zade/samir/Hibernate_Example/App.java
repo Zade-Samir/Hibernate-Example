5845d3f635f65ca4d98dd59aa4c53dba235d845e
sbanduji <sbanduji@10.236.6.214>
2025-08-22 16:33:38 +0530

    Session and SessionFactory Created

--- Hibernate-Example/src/main/java/zade/samir/Hibernate_Example/App.java
+++ Hibernate-Example/src/main/java/zade/samir/Hibernate_Example/App.java
@@ -1,9 +1,26 @@
 package zade.samir.Hibernate_Example;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ){
+        
+    	Student s1 = new Student(); 
+    	
+    	s1.setRollNo(1);
+    	s1.setsName("Samir");
+    	s1.setsAge(23);
+    	
+    	Configuration cfg = new Configuration();
+    	//build the session factory and return the sessionFactory Object
+    	SessionFactory sf = cfg.buildSessionFactory();  
+    	Session session = sf.openSession(); //open the session for connection
+    	
+    	session.persist(s1);
+    	
+    	System.out.println(session);
     }
 }
