40ee2c7119995fc0ce912c9851a28e24a9fee569
sbanduji <sbanduji@10.236.6.214>
2025-08-22 20:36:22 +0530

    Changing table and column names

---
+++ Hibernate-Example/src/main/java/zade/samir/Hibernate_Example/Alien.java
@@ -0,0 +1,46 @@
+package zade.samir.Hibernate_Example;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
+
+@Entity
+@Table(name = "alien_table") //change the table name
+public class Alien {
+	
+	@Id
+	private int aid;
+	
+	@Column(name = "alien_name") //change the column name
+	private String aname;
+	
+	@Transient //to hide this below from database or ignore to make columns
+	private String tech;
+	public int getAid() {
+		return aid;
+	}
+	public void setAid(int aid) {
+		this.aid = aid;
+	}
+	public String getAname() {
+		return aname;
+	}
+	public void setAname(String aname) {
+		this.aname = aname;
+	}
+	public String getTech() {
+		return tech;
+	}
+	public void setTech(String tech) {
+		this.tech = tech;
+	}
+	@Override
+	public String toString() {
+		return "Alien [aid=" + aid + ", aname=" + aname + ", tech=" + tech + "]";
+	}
+	
+	
+
+}
